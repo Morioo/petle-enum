f937f8576c7676f3060ff74b4c5481230ed4c92f
Morioo <107799693+Morioo@users.noreply.github.com>
2024-09-03 14:19:28 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+
+public enum NetEnums
+{
+
+    Facebook,
+    linkedin,
+    Twitter,
+    Instagarm
+
+}
+
+enum DaysOfWeek
+{
+    Mondey,
+    tuseday,
+    Wednesday,
+    Thrusday,
+    Friday,
+    Saturday,
+    sunday
+
+}
+
+
+class program {
+
+    static void Main(string[] args) {
+
+       
+        foreach (var name in Enum.GetNames(typeof(NetEnums)))
+        {
+
+            Console.WriteLine(name);
+
+
+        }
+        foreach (var val in Enum.GetValues(typeof(NetEnums)))
+                {
+
+                    Console.WriteLine((int)val);
+                }
+        
+    }
+    //Konwersja string do enum
+
+    
+  
+
+
+
+
+
+}
